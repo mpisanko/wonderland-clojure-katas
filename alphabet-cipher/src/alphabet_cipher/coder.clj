03a7e99ac51978df95ee29ba5c37de3261a11a66
michal pisanko <mpisanko@gmail.com>
2015-02-10 22:03:07 +1100

    solve alpha cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,30 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+(def alphabet (into [] "abcdefghijklmnopqrstuvwxyz"))
 
-(defn decode [keyword message]
-  "decodeme")
+(def dict (zipmap alphabet (iterate inc 0)))
+
+(defn code 
+  [f [k m]]
+  (alphabet 
+    (mod (f (dict k) (dict m)) (count alphabet))))
+
+(defn key-msg
+  [keyword message]
+  (into [] (map vector keyword message)))
+
+(defn perform-coding
+  [summation-fn msg-1 msg-2]
+  (apply
+    str
+    (map
+      (partial code summation-fn)
+      (key-msg msg-1 msg-2))))
+
+(defn encode 
+  [keyword message]
+  (perform-coding + (cycle keyword) message))
+
+(defn decode 
+  [keyword message]
+  (perform-coding - message (cycle keyword)))
