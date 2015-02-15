606b937aa6eeead0817328afaa74d38880e1c755
michal pisanko <mpisanko@gmail.com>
2015-02-15 19:01:54 +1100

    refactor comparing cards

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -11,21 +11,20 @@
 (def suit-values (zipmap suits (iterate inc 0)))
 (def rank-values (zipmap ranks (iterate inc 0)))
 
-(defn compare-suits
-  [[suit-1 _ :as card-1] [suit-2 _ :as card-2]]
-  (let [sub (- (suit-values suit-1) (suit-values suit-2))]
-    (cond 
-      (pos? sub) [[card-1 card-2] []]
-      (neg? sub) [[] [card-1 card-2]]
-      :else [[card-1] [card-2]])))
 
-(defn compare-ranks
-  [[_ rank-1 :as card-1] [_ rank-2 :as card-2]]
-  (let [sub (- (rank-values rank-1) (rank-values rank-2))]
-    (cond 
-      (zero? sub) (compare-suits card-1 card-2)
-      (pos? sub) [[card-1 card-2] []]
-      :else [[] [card-1 card-2]])))
+(defn compare-cards
+  [[suit-1 rank-1 :as card-1] [suit-2 rank-2 :as card-2]]
+  (let [rank-comp (compare (rank-values rank-1) (rank-values rank-2))
+        suit-comp (compare (suit-values suit-1) (suit-values suit-2))]
+    (if (zero? rank-comp) 
+      suit-comp
+      rank-comp)))
+
+(defn compare-and-distribute
+  [card-1 card-2]
+  (if (pos? (compare-cards card-1 card-2))
+    [[card-1 card-2] []]
+    [[] [card-1 card-2]]))
 
 (defn deal
   [cards]
@@ -45,7 +44,7 @@
 (defn play-round [player1-cards player2-cards]
   (let [player1-card (peek player1-cards)
         player2-card (peek player2-cards)
-        [player1-round player2-round] (compare-ranks player1-card player2-card)]
+        [player1-round player2-round] (compare-and-distribute player1-card player2-card)]
     [(append-unless-empty (pop player1-cards) player1-round) 
      (append-unless-empty (pop player2-cards) player2-round)]))
 
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -12,17 +12,17 @@
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
     (testing "queens are higer rank than jacks"
-      (is (= [[["_" :queen] ["_" :jack]][]]) (compare-ranks ["_" :queen] ["_" :jack])))
+      (is (pos? (compare-cards [:club :queen] [:spade :jack]))))
     (testing "kings are higer rank than queens"
-      (is (= [[["_" :king] ["_" :queen]][]]) (compare-ranks ["_" :king] ["_" :queen])))
+      (is (pos? (compare-cards [:diamond :king] [:heart :queen]))))
     (testing "aces are higer rank than kings"
-      (is (= [[["_" :ace] ["_" :king]][]]) (compare-ranks ["_" :ace] ["_" :king])))
+      (is (pos? (compare-cards [:spade :ace] [:heart :king]))))
     (testing "if the ranks are equal, clubs beat spades"
-      (is (= [[[:club :ace] [:spade :ace]][]]) (compare-ranks [:club :ace] [:spade :ace])))
+      (is (pos? (compare-cards [:club :ace] [:spade :ace]))))
     (testing "if the ranks are equal, diamonds beat clubs"
-      (is (= [[[:diamond 5] [:club 5]][]]) (compare-ranks [:diamond 5] [:club 5])))
+      (is (pos? (compare-cards [:diamond 5] [:club 5]))))
     (testing "if the ranks are equal, hearts beat diamonds"
-      (is (= [[[:heart 10] [:diamond 10]][]]) (compare-ranks [:heart 10] [:diamond 10])))))
+      (is (pos? (compare-cards [:heart 10] [:diamond 10]))))))
 
 (deftest test-play-round
   (testing "play round gives both cards to the side which had higher card"
